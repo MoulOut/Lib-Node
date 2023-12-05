7003416b290c2e64e11b2dcc450a111c1cf4d961
MoulOut <mmoulinlima@outlook.com>
2023-12-05 17:02:42 -0300

    Tratamento de erros

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
+import fs from 'fs';
 import chalk from 'chalk';
-console.log(chalk.blueBright('olá mundo'));
+
+function errorSolve(erro) {
+    console.log(erro)
+  throw new Error(chalk.red(erro.code,'Arquivo não encontrado'));
+}
+
+function getFile(fileOrigin) {
+  const encondig = 'utf-8';
+  fs.readFile(fileOrigin, encondig, (erro, text) => {
+    if (erro) {
+      errorSolve(erro);
+    }
+    console.log(chalk.green(text));
+  });
+}
+
+getFile('./arquivos/texto.md');
